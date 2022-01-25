5d9908207462705375aaafe49fdb085b1469e693
Sanchit Banati <sanchitbanati@Sanchits-Mac-mini.local>
2022-01-25 23:40:21 +0530

    Local Running

--- src/views/SpaceSettings.vue
+++ src/views/SpaceSettings.vue
@@ -63,7 +63,7 @@ const form = ref({
 const validate = computed(() => {
   if (form.value.terms === '') delete form.value.terms;
   if (form.value.avatar === '') delete form.value.avatar;
-  
+
   return validateSchema(schemas.space, form.value);
 });
 
